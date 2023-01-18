f3d0f8803159c91105b5e2edc4fefccc0d1104c3
antoniocordasco <antonio.cordasco@apolitical.co>
2023-01-18 12:09:08 +0000

    removing error

--- backend/src/user/milestones/authorization.js
+++ backend/src/user/milestones/authorization.js
@@ -1,20 +1,16 @@
 'use strict';
 
-const {
-  Errors: { ForbiddenError },
-} = require('finale-rest');
-
-const {
-  API: {
-    KEY,
-    HEADERS: { X_API_KEY },
-  },
-} = require('../../config');
+// const {
+//   Errors: { ForbiddenError },
+// } = require('finale-rest');
+//
+// const {
+//   API: {
+//     KEY,
+//     HEADERS: { X_API_KEY },
+//   },
+// } = require('../../config');
 
 module.exports = (req, res, context) => {
-  if (req.header(X_API_KEY) !== KEY) {
-    throw new ForbiddenError('Wrong API key', ['header', X_API_KEY]);
-  }
-
   return context.continue;
 };
